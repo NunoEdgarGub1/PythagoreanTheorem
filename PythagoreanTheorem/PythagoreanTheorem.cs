94c4d114ba8b89df1917c6e1d50b88fc326dd316
NunoEdgarGub1 <nunoedgar.fernandes2@gmail.com>
2015-10-02 21:07:14 +0100

    init commit Pythagorean

---
+++ PythagoreanTheorem/PythagoreanTheorem.cs
@@ -0,0 +1,20 @@
+﻿using static System.Math;
+
+namespace Syncfusion
+{
+
+    public class Calc
+    {
+       
+
+
+        public static double Pythagorean(double a, double b)
+       
+        {
+            void Main()
+
+        double cSquared = Pow(a, 2) + Pow(b, 2);
+            return Sqrt(cSquared);
+        }
+    }
+}
